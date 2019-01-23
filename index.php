b268046218a4befbca395c402563e4e43ff7221f
Nelito <ngvjqp@gmail.com>
2019-01-23 04:12:44 -0300

    Individual pages

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php require_once("includes/header.php"); ?>
+  <body>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar ">
+      <?php require_once("includes/app-header.php"); ?>
+      <div class="app-main">
+        <?php require_once("includes/sidebar.php"); ?>
+        <div class="app-main__outer">
+          <div class="app-main__inner">
+            <div class="app-page-title">
+              <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                  <div class="page-title-icon">
+                    <i class="fa fa-dice icon-gradient bg-mean-fruit">
+                                        </i>
+                  </div>
+                  <div>
+                    <div class="">
+                      "Diligência é a mãe da boa sorte." <small> - Benjamin Franklin</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 pb-2 text-center">
+                <a href="https://go.hotmart.com/L11280144G" target="_blank"><img src="images/banner-aposte-melhor.gif" width="100%" style="max-width: 600px;"/></a>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12 col-md-6">
+                <div class="card mb-3 widget-content bg-grow-early">
+                  <div class="widget-content-wrapper text-white">
+                    <div class="" id="mega-sena">
+                      <a class="h2" href="/megasena">Mega-sena - <span class="numero"></span></a>
+                      <p class="card-text">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6">
+                <div class="card mb-3 widget-content bg-midnight-bloom">
+                  <div class="widget-content-wrapper text-white">
+                    <div class="" id="quina">
+                      <a class="h2" href="/quina">Quina - <span class="numero"></span></a>
+                      <p class="card-text">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6">
+                <div class="card mb-3 widget-content bg-love-kiss">
+                  <div class="widget-content-wrapper text-white">
+                    <div class="" id="lotofacil">
+                      <a class="h2" href="/lotofacil">Lotofácil - <span class="numero"></span></a>
+                      <p class="card-text">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                      <a target="_blank" class="btn btn-primary text-center d-block bg-happy-green border-0" href="https://go.hotmart.com/L11280144G">Melhore suas apostas na Lotofácil</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6">
+                <div class="card mb-3 widget-content bg-happy-itmeo">
+                  <div class="widget-content-wrapper text-white">
+                    <div class="" id="timemania">
+                      <a class="h2" href="/timemania">Timemania - <span class="numero"></span></a>
+                      <p class="card-text">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                        <span class="time">Time: </span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6">
+                <div class="card mb-3 widget-content bg-strong-bliss">
+                  <div class="widget-content-wrapper text-white">
+                    <div class="" id="lotomania">
+                      <a class="h2" href="/lotomania">Lotomania - <span class="numero"></span></a>
+                      <p class="card-text">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6">
+                <div class="card mb-3 widget-content bg-grow-early">
+                  <div class="widget-content-wrapper text-white">
+                    <div class="" id="dupla-sena">
+                      <a class="h2" href="/duplasena">Dupla-sena - <span class="numero"></span></a>
+                      <p class="card-text">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+<?php require_once("includes/footer.php"); ?>
---
+++ lotofacil.php
@@ -0,0 +1,45 @@
+<?php require_once("includes/header.php"); ?>
+  <body>
+    <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar ">
+      <?php require_once("includes/app-header.php"); ?>
+      <div class="app-main">
+        <?php require_once("includes/sidebar.php"); ?>
+        <div class="app-main__outer">
+          <div class="app-main__inner">
+            <div class="app-page-title">
+              <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                  <div class="page-title-icon">
+                    <i class="fa fa-dice icon-gradient bg-mean-fruit">
+                                        </i>
+                  </div>
+                  <div>
+                    <div class="">
+                      "A quantidade de boa sorte em seu caminho depende da sua disposição para agir." <small> - Barba Sher</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="card mb-3 widget-content">
+                  <div class="widget-content-wrapper">
+                    <div class="" id="lotofacil">
+                      <h1 class="">Lotofácil - <span class="numero"></span></h1>
+                      <p class="card-text h3">
+                        <span class="acumulado">Acumulado: R$ </span><br>
+                        <span class="dezenas"></span>
+                        <span class="ganhadores"></span>
+                      </p>
+                      <span class="dia">Dia: </span><br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <a target="_blank" class="btn btn-primary text-center bg-happy-green border-0" href="https://go.hotmart.com/L11280144G">Melhore suas apostas na Lotofácil</a>
+              </div>
+            </div>
+          </div>
+<?php require_once("includes/footer.php"); ?>
