71e569c471826b2b8d6429eeefe79092fc68e43a
Nelito <ngvjqp@gmail.com>
2019-02-04 02:07:13 -0300

    gtm e amplitude

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,6 +1,16 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121306349-3"></script>
+    <script>
+     window.dataLayer = window.dataLayer || [];
+     function gtag() {
+       dataLayer.push(arguments);
+     }
+     gtag('js', new Date());
+     gtag('config', 'UA-121306349-3');
+    </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -86,6 +96,42 @@ a.appendChild(r);
 
 </script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script type="text/javascript">
+ (function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
+ ;r.type="text/javascript";r.async=true
+ ;r.src="https://cdn.amplitude.com/libs/amplitude-4.4.0-min.gz.js"
+ ;r.onload=function(){if(e.amplitude.runQueuedFunctions){
+ e.amplitude.runQueuedFunctions()}else{
+ console.log("[Amplitude] Error: could not load SDK")}}
+ ;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
+ ;function s(e,t){e.prototype[t]=function(){
+ this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}
+ var o=function(){this._q=[];return this}
+ ;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
+ ;for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[]
+ ;return this}
+ ;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
+ ;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
+ ;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","logEventWithTimestamp","logEventWithGroups","setSessionId","resetSessionId"]
+ ;function v(e){function t(t){e[t]=function(){
+ e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
+ for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){
+ e=(!e||e.length===0?"$default_instance":e).toLowerCase()
+ ;if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]}
+ ;e.amplitude=n})(window,document);
+ amplitude.getInstance().init("2d8e638b11b6ac75b1677e849ca350bc");
+    amplitude.getInstance().logEvent('Page Loaded', {
+      'page': window.location.pathname.substring(1,window.location.pathname.length-1)
+    });
+     jQuery(document).ready(function($) {
+ $("a").click(function () {
+   amplitude.getInstance().logEvent('clicked', {
+     'text': $(this).text(),
+     'href': $(this).attr("href")
+   });
+ });
+});
+   </script>
 </body>
 
 </html>
